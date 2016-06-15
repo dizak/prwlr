b07acdb106ab9c6008a3533b3cbde3a9d90d4209
Dariusz Izak <dariusz.izak@ibb.waw.pl>
2016-06-15 12:00:43 +0200

    padnas says somethg about scalar vals and index...

--- P_CRAWLER.py
+++ P_CRAWLER.py
@@ -698,13 +698,9 @@ class Ortho_Stats:
         self.query_species_stats = query_species_Genome
         self.inter_df_stats = inter_df_Ortho_Interactions
         self.gene_profiles_stats = gene_profiles_Genome
-        self.tot_inter_num = None
-        self.DMF_positive_num = None
-        self.DMF_negative_num = None
-        self.sim_prof_num = None
+        self.num_prop_res = None
         self.e_value = None
         self.perm_results = None
-        self.test_last_df_test = None
 
     def df_selector(self,
                     DMF = "positive",
@@ -799,12 +795,13 @@ class Ortho_Stats:
                            in_prof_sim_lev) &\
                           (self.inter_df_stats["Profiles_similarity_score"] > 0)
         mir_prof_bool = (self.inter_df_stats["Profiles_similarity_score"] == 0)
-        self.tot_inter_num = len(self.inter_df_stats)
-        self.DMF_positive_num = len(self.inter_df_stats[positive_DMF_bool])
-        self.DMF_negative_num = len(self.inter_df_stats[negative_DMF_bool])
-        self.sim_prof_num = len(self.inter_df_stats[sim_prof_bool])
-        self.unsim_prof_num = len(self.inter_df_stats[unsim_prof_bool])
-        self.mir_prof_num = len(self.inter_df_stats[mir_prof_bool])
+        num_prop_res_temp = {"total_interactions_number": len(self.inter_df_stats),
+                             "DMF_positive_number": len(self.inter_df_stats[positive_DMF_bool]),
+                             "DMF_negative_number": len(self.inter_df_stats[negative_DMF_bool]),
+                             "similar_profiles_number": len(self.inter_df_stats[sim_prof_bool]),
+                             "unsimilar_profiles_number": len(self.inter_df_stats[unsim_prof_bool]),
+                             "mirror_profiles_number": len(self.inter_df_stats[mir_prof_bool])}
+        self.num_prop_res = num_prop_res_temp
 
     def names_perm(self,
                    e_value,
