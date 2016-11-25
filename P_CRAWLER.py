983d62f6bb8609783bc06eee1a43223824f125c1
Dariusz Izak <dariusz.izak@ibb.waw.pl>
2016-11-25 11:42:22 +0100

    ENH@get_org_db_X_ref:Opt for stripping kegg_id prefix

--- P_CRAWLER.py
+++ P_CRAWLER.py
@@ -1431,7 +1431,8 @@ class KEGG_API:
                          target_db,
                          out_file_name,
                          skip_dwnld = False,
-                         strip_ORF_pref = True):
+                         strip_ORF_prefix = True,
+                         strip_kegg_id_prefix = False):
         """Get desired KEGG's database entries linked with all the genes from
         given organism. Data are downloaded to a local file and then made into
         pandas.DataFrame. File can be reused. Necessary for
@@ -1458,11 +1459,16 @@ class KEGG_API:
                 fout.write(res.content)
         self.org_db_X_ref_df = pd.read_csv(out_file_name,
                                            names = ["ORF_id", "kegg_id"],
-                                           header=None,
+                                           header = None,
                                            sep = "\t")
-        if strip_ORF_pref == True:
+        if strip_ORF_prefix == True:
             self.org_db_X_ref_df["ORF_id"] = self.org_db_X_ref_df["ORF_id"].replace({"{0}:".format(org_id): ""},
                                                                                     regex = True)
+        else:
+            pass
+        if strip_kegg_id_prefix == True:
+            self.org_db_X_ref_df["kegg_id"] = self.org_db_X_ref_df["kegg_id"].replace({"{0}:".format(self.databases[target_db]): ""},
+                                                                                      regex=True)
         else:
             pass
 
