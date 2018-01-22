34e812e9b3040b73bff04de19a8d2fc0aa4c63ab
dizak <dariusz.izak@ibb.waw.pl>
2018-01-22 11:45:14 +0100

    ENH:Added ProfileLengthError

--- prowler/errors.py
+++ prowler/errors.py
@@ -6,3 +6,10 @@ class ParserError(Exception):
     Inappropriate structure passed to parser.
     """
     pass
+
+
+class ProfileLengthError(Exception):
+    """
+    Different length of the reference and query.
+    """
+    pass
