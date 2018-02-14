b4f3d415a0eb430785651e7481b8e71941e2db37
dizak <dariusz.izak@ibb.waw.pl>
2018-02-14 17:26:27 +0100

    FIX@permute_profiles

--- prowler/stats.py
+++ prowler/stats.py
@@ -208,7 +208,7 @@ class Stats(_DatabasesColumns,
             sim_prof_perm_num = len(qa_merged_score_df[sim_prof_bool])
             unsim_prof_perm_num = len(qa_merged_score_df[unsim_prof_bool])
             mir_prof_perm_num = len(qa_merged_score_df[mir_prof_bool])
-            del q_ORF_prof_df, a_ORF_prof_df, drop_prof_temp_df, stack_ORF_prof_df, stack_prof_perm_df, q_merged_df, qa_merged_df, qa_merged_score_df
+            del q_ORF_prof_df, a_ORF_prof_df, drop_prof_temp_df, stack_ORF_prof_df, stack_prof_perm_df, q_merged_df, qa_merged_df
             gc.collect()
             if store_dataframe is True:
                 return {"similar": sim_prof_perm_num,
