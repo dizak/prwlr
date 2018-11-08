640bad87a68cebe9f774b9c3222dbce11750a7ae
dizak <dariusz.izak@ibb.waw.pl>
2018-11-08 15:26:52 +0100

    Missing coma

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
         'pathway',
         'kegg',
         'evolution',
-    ]
+    ],
     classifiers=[
         "License :: OSI Approved :: BSD License",
         "Operating System :: POSIX :: Linux",
