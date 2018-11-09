d86ceeeaf91aa7e812331b7f6cd3f776d1aa1db6
Dariusz Izak <dariusz.izak@ibb.waw.pl>
2018-11-09 15:07:26 +0100

    Enh#12 (#76)
    * Initial commit
    
    * Added most options from mothulity
    
    * Initial commit
    
    * Adapted travis to pip-package based testing
    
    * Missing coma
    
    * Bad line, remanant from the conda env file
    
    * Missing find_packages
    
    * Try except for imports so that __author__ and __version__ can be imported before deps are instaled
    
    * Initial commit
    
    * Using setup from distutils
    
    * Moved __version__ and __author__ from __init__
    
    * import __version__ and __author__ from separate files
    
    * Temporarily commented out
    
    * Removed comments
    
    * Initial commit
    
    * Removed
    
    * Hard-coded author and version from git tag
    
    * Obscure numpy import
    
    * Indentation
    
    * Update to obscure numpy import
    
    * Version hard-coded
    
    * Trying once more with the conditional imports in __init__
    
    * Redundant spaces
    
    * Testing quit on ImportError
    
    * Specified python3 versions in detail
    
    * Initial commit
    
    * Moved everything defined in __init__ so far to core.py
    
    * Just print not warning
    
    * Python version reuquirements compliant with scipy
    
    * Removed python 3.3 since incompatible with scipy. Removed python 3.7 since TravisCI does not handle it
    
    * Changed pseudo-warning message
    
    * databases import was missing
    
    * Added deploy commands

--- setup.py
+++ setup.py
@@ -1,13 +1,39 @@
-from setuptools import setup
-from prwlr import __version__ as VERSION
-from prwlr import __author__ as AUTHOR
+from distutils.core import setup
+from setuptools import find_packages
+from prwlr import __version__
+from prwlr import __author__
 
 
-setup(name="prwlr",
-      version=VERSION,
-      description="Module for annotatating genetic interactions networks with phylogenetic profiles.",
-      author=AUTHOR,
-      author_email="dariusz.izak@ibb.waw.pl",
-      url="https://github.com/dizak/prwlr",
-      license="MIT",
-      py_modules=["prwlr"])
+VERSION = __version__
+AUTHOR = __author__
+
+setup(
+    name="prwlr",
+    version=VERSION,
+    author=AUTHOR,
+    packages=find_packages(exclude=["*test*"]),
+    install_requires=open("requirements.txt").readlines(),
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, <4',
+    long_description=open("README.md").read(),
+    description="Annotate genetic interactions networks with phylogenetic profiles and other attributes.",
+    author_email="dariusz.izak@ibb.waw.pl",
+    url="https://github.com/dizak/prwlr",
+    license="BSD",
+    keywords=[
+        'genetic',
+        'interactions',
+        'network',
+        'phylogenetic',
+        'profile',
+        'module',
+        'pathway',
+        'kegg',
+        'evolution',
+    ],
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+    ],
+)
